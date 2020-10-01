7c1f18dcc9dd52d9912e1902d53dc99eb7424908
Murad Faridi <muradfaridi92@gmail.com>
2020-10-01 07:53:25 +0500

    hello world

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-serviceWorker.unregister();
+function Hi(){
+	return <div>Hello World!</div>
+}
