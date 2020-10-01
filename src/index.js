b5469b2c29bd944c4d2b6745affe545abd9dbe99
Murad Faridi <muradfaridi92@gmail.com>
2020-10-01 10:27:50 +0500

    assignment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 function Hi(){
-	return <div>Hello World!</div>
+	return 	<div>
+				Hello <strong>Murad!</strong>
+				<h3>Your Work List:</h3>
+				<ul>
+					<li>SASA Subscription</li>
+					<li>Handle Orders</li>
+					<li>Distribute DSB</li>
+					<li>Handle Products</li>
+				</ul>
+			</div>
 }
 
 ReactDOM.render(<Hi />,document.getElementById('root'));
