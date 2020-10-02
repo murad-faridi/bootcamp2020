5571480dd08501f29fd5f76e141151b1b6c34b92
Murad Faridi <muradfaridi92@gmail.com>
2020-10-02 11:07:24 +0500

    Props and State

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const MediaCard = (props) => (
-	 		<div>
-				<h2>{props.title}</h2>
-				<p>{props.body}</p>
-				{props.isOpen ? <div>Opened</div> : <div>Closed</div>}
-				<div>{props.imageUrl}</div>
-			</div>
-)
+// const MediaCard = (props) => (
+// 	 		<div>
+// 				<h2>{props.title}</h2>
+// 				<p>{props.body}</p>
+// 				{props.isOpen ? <div>Opened</div> : <div>Closed</div>}
+// 				<div>{props.imageUrl}</div>
+// 			</div>
+// )
+
+// ReactDOM.render(<MediaCard 
+// 	title="Welcome" 
+// 	body={<div>This is <b>Body</b></div>} 
+// 	imageUrl={<img src="https://technowise360.com/assets/catalog/81608.jpg" style={{width: "200px"}} />}
+// 	isOpen={true}
+// 	/>,document.getElementById('root'));
 
 const Room = () => {
 	const[isLit, setLit] = React.useState(false);
@@ -28,14 +35,6 @@ const Room = () => {
 	</div>
 	)
 }
-
-
-// ReactDOM.render(<MediaCard 
-// 	title="Welcome" 
-// 	body={<div>This is <b>Body</b></div>} 
-// 	imageUrl={<img src="https://technowise360.com/assets/catalog/81608.jpg" style={{width: "200px"}} />}
-// 	isOpen={true}
-// 	/>,document.getElementById('root'));
 
 
 ReactDOM.render(<Room />,document.getElementById('root'));
