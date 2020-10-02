4a03fbeebdb5329e09636da73a0c477cc96883d3
Murad Faridi <muradfaridi92@gmail.com>
2020-10-02 10:56:25 +0500

    State and Props

--- src/index.js
+++ src/index.js
@@ -2,17 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hi(){
-	return 	<div>
-				Hello <strong>Murad!</strong>
-				<h3>Your Work List:</h3>
-				<ul>
-					<li>SASA Subscription</li>
-					<li>Handle Orders</li>
-					<li>Distribute DSB</li>
-					<li>Handle Products</li>
-				</ul>
+const MediaCard = (props) => (
+	 		<div>
+				<h2>{props.title}</h2>
+				<p>{props.body}</p>
+				{props.isOpen ? <div>Opened</div> : <div>Closed</div>}
+				<div>{props.imageUrl}</div>
 			</div>
+)
+
+const Room = () => {
+	const[isLit, setLit] = React.useState(false);
+	let[temp, setTemp] = React.useState(22);
+	return (
+	<div className={`room ${isLit ? "lit" : "dark"}`}>
+		The Room is {isLit ? 'Lit' : 'Dark'}
+		<br />
+		<button onClick={()=>setLit(true)}>ON</button>
+		<button onClick={()=>setLit(false)}>OFF</button>
+		<br /><br />
+		Room Temp: {temp}
+		<br />
+		<button onClick={()=>setTemp(++temp)}>+</button>
+		<button onClick={()=>setTemp(--temp)}>-</button>
+	</div>
+	)
 }
 
-ReactDOM.render(<Hi />,document.getElementById('root'));
+
+// ReactDOM.render(<MediaCard 
+// 	title="Welcome" 
+// 	body={<div>This is <b>Body</b></div>} 
+// 	imageUrl={<img src="https://technowise360.com/assets/catalog/81608.jpg" style={{width: "200px"}} />}
+// 	isOpen={true}
+// 	/>,document.getElementById('root'));
+
+
+ReactDOM.render(<Room />,document.getElementById('root'));
